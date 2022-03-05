13a1e2296d1756e4ea41941f1ceb241e4ef11624
MansiSinghal06 <92847041+MansiSinghal06@users.noreply.github.com>
2022-03-05 22:53:30 +0530

    Create Main.dart

---
+++ Main.dart
@@ -0,0 +1,122 @@
+import 'data.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BookRecommender());
+
+class BookRecommender extends StatefulWidget {
+  const BookRecommender({Key? key}) : super(key: key);
+
+  @override
+  _BookRecommenderState createState() => _BookRecommenderState();
+}
+
+class _BookRecommenderState extends State<BookRecommender> {
+  String input = "";
+  final myController = TextEditingController();
+
+  get fit => null;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.grey[900],
+            appBar: AppBar(
+              title: const Text("Book Recommender"),
+              centerTitle: true,
+              backgroundColor: Colors.grey[850],
+            ),
+            body: new Stack(children: <Widget>[
+              new Container(
+                decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                    image: new AssetImage("Books.jpeg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              ListView(children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 10,
+                      ),
+                      child: TextField(
+                          controller: myController,
+                          decoration: const InputDecoration(
+                              labelText: "Enter your preferred Genre"),
+                          style:
+                              TextStyle(color: Color.fromARGB(255, 12, 0, 5))),
+                    ),
+                    ElevatedButton(
+                      child: const Text("Show us the relevant searches"),
+                      onPressed: () {
+                        setState(() {
+                          input = myController.text;
+                        });
+                        myController.clear();
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Text(Booksuggestions[input]!),
+                  ],
+                ),
+
+                //children: [
+                Row(children: [
+                  Container(
+                      margin: EdgeInsets.only(left: 60, right: 50),
+                      padding: new EdgeInsets.all(20.0),
+                      child: new Column(children: <Widget>[
+                        Padding(
+                            padding: EdgeInsets.all(50),
+                            // ignore: unnecessary_new
+                            child: new ButtonBar(
+                              mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                ElevatedButton(
+                                  child: new Text('Young Adult'),
+                                  // color: Colors.lightGreen,
+                                  onPressed: () {/** */},
+                                ),
+                                ElevatedButton(
+                                  child: const Text('Mystery'),
+                                  // color: Colors.lightGreen,
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        //    input = myController.text();
+                                      },
+                                    );
+                                    myController.clear();
+                                  },
+                                ),
+                                ElevatedButton(
+                                  child: const Text('Action/Thriller'),
+                                  // color: Colors.lightGreen,
+                                  onPressed: () {/** */},
+                                ),
+                                ElevatedButton(
+                                  child: const Text('Romance'),
+                                  // color: Colors.lightGreen,
+                                  onPressed: () {/** */},
+                                ),
+                                ElevatedButton(
+                                  child: const Text('Poetry'),
+                                  // color: Colors.lightGreen,
+                                  onPressed: () {/** */},
+                                ),
+                                ElevatedButton(
+                                  child: const Text('Classics'),
+                                  // color: Colors.lightGreen,
+                                  onPressed: () {/** */},
+                                ),
+                              ],
+                            ))
+                      ]))
+                ]),
+              ]),
+            ])));
+  }
+}
