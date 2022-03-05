397dd0e2c938a32e1cc2724ae8f98474462ed8a2
Puneeta Sapra <89627009+PuneetaSapra@users.noreply.github.com>
2022-03-05 22:52:47 +0530

    Create data.dart

---
+++ data.dart
@@ -0,0 +1,15 @@
+Map<String, String> Booksuggestions = {
+  "": "Enter a genre to see the books suggestions",
+  "Romance": 
+      "Me Before you \n\n  The Fault in our Stars \n\n Eleanor and Park",
+  "Classics":
+      "Pride & Prejudice \n\n Emma \n\n Wuthering heights \n\n David Copperfield",
+  "Young Adult":
+      "The Fault In Our Stars \n\n Looking For Alaska \n\n Eleanor and Park \n\n Me Before You",
+  "Action Thriller":
+      "The Book Thief \n\n If Tomorrow Comes \n\n The Vinci Code \n\n Harry Potter",
+  "Mystery":
+      "Sherlock Holmes \n\n Murder on the Orient Express \n\n Gone Girl \n\n The cuckoo's Calling",
+  "Poetry":
+      "The Sun and Her Flowers \n\n  Rumi's Little Book of Life \n\n Leaves of Grass \n\n Milk and Honey \n\n Pillow Thoughts",
+};
